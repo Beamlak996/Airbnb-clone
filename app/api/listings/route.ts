97717eff3c02fb6e175dab3fd6bcada1b474b01d
Beamlak996 <beamlaktsegaye996@gmail.com>
2023-09-04 21:29:48 +0300

    final

--- app/api/listings/route.ts
+++ app/api/listings/route.ts
@@ -1,13 +1,16 @@
 import { NextResponse } from "next/server";
-import prisma from "@/app/libs/prismadb";
-import { getCurrentUser } from "@/app/actions/getCurrentUser";
 
-export async function POST(req: Request) {
+import prisma from "@/app/libs/prismadb";
+import {getCurrentUser} from "@/app/actions/getCurrentUser";
+
+export async function POST(request: Request) {
   const currentUser = await getCurrentUser();
+
   if (!currentUser) {
     return NextResponse.error();
   }
-  const body = await req.json();
+
+  const body = await request.json();
   const {
     title,
     description,
@@ -19,6 +22,13 @@ export async function POST(req: Request) {
     location,
     price,
   } = body;
+
+  Object.keys(body).forEach((value: any) => {
+    if (!body[value]) {
+      NextResponse.error();
+    }
+  });
+
   const listing = await prisma.listing.create({
     data: {
       title,
@@ -30,8 +40,9 @@ export async function POST(req: Request) {
       guestCount,
       locationValue: location.value,
       price: parseInt(price, 10),
-      userId: currentUser.id
+      userId: currentUser.id,
     },
   });
-  return NextResponse.json(listing)
+
+  return NextResponse.json(listing);
 }
